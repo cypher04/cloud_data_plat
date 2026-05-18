63b9218d8d0614fc9e19eb338b4002ef3c1766a1
Seyi Abiodun <cypher04>
2026-05-18 11:43:58 +0100

    Resource(datalake): Deployed data lake gen2 to infra.

---
+++ modules/data-lake/output.tf
@@ -0,0 +1,7 @@
+output "storage_account_id" {
+    value = azurerm_storage_account.cloudstorage.id
+}
+
+output "storage_datalake_gen2_id" {
+    value = azurerm_storage_data_lake_gen2_filesystem.cloudfilesystem.id
+}
