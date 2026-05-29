693dacb0d36eae21ebe078ed89848e590e072f8f
Seyi Abiodun <cypher04>
2026-05-29 02:59:15 +0100

    feature(module): Created module for hdinsights, keyvault in root file. Created new workspace.

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -33,3 +33,49 @@ variable "admin_password" {
   type        = string
   sensitive   = true
 }
+
+
+variable "kafka_gateway_name" {
+  description = "The name of the Kafka gateway"
+  type        = string
+}
+
+variable "kafka_gateway_password" {
+  description = "The password for the Kafka gateway"
+  type        = string
+  sensitive   = true
+}
+
+variable "kafka_head_node_name" {
+  description = "The name of the Kafka head node"
+  type        = string
+}
+
+variable "kafka_head_node_password" {
+  description = "The password for the Kafka head node"
+  type        = string
+  sensitive   = true
+}
+
+variable "kafka_worker_node_name" {
+  description = "The name of the Kafka worker node"
+  type        = string
+}
+
+variable "kafka_worker_node_password" {
+  description = "The password for the Kafka worker node"
+  type        = string
+  sensitive   = true
+}
+
+variable "kafka_zookeeper_node_name" {
+  description = "The name of the Kafka zookeeper node"
+  type        = string
+}
+
+variable "kafka_zookeeper_node_password" {
+  description = "The password for the Kafka zookeeper node"
+  type        = string
+  sensitive   = true
+}
+
